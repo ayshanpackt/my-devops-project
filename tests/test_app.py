bb6424c525c423a0e2625f86ce63d53a64f4f641
Aysha Nadeem <ayshan@packt.com>
2026-02-11 13:06:47 +0530

    ci: fix test message to match app output

--- tests/test_app.py
+++ tests/test_app.py
@@ -8,4 +8,4 @@ def test_hello():
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
-    assert b"Hello from inside a Docker Container!" in response.data
+    assert b"Hello from Docker Compose!" in response.data
